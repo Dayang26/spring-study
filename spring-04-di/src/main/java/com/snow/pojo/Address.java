6bee876e95ef2a318dd81e0c9a5b0f499a30f4b5
Dayang26 <hsyy26@qq.com>
2020-12-16 20:20:00 +0800

    123

---
+++ spring-04-di/src/main/java/com/snow/pojo/Address.java
@@ -0,0 +1,17 @@
+package com.snow.pojo;
+
+/**
+ * @author Snow
+ * @create 2020-12-16 20:13
+ */
+public class Address {
+    private String address;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
